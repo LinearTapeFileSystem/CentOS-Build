d1e9e1c26a23502c14566196b6a8b87841d11775
Atsushi Abe <piste@jp.ibm.com>
2019-12-24 17:45:32 +0900

    Add I/F checking

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,12 @@
 ./configure --prefix=$1
 make
 make install
+
+# Backend I/F checking
+cd $1
+git clone https://github.com/LinearTapeFileSystem/ltfs-backends.git
+cd ltfs-backends
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:/tmp/ltfs/lib/pkgconfig
+./autogen.sh
+./configure --enable-checkonly
+make
